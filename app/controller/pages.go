99ae3f93465a388da2e6d2ba250ae360f8ae48a0
maxXvostik <xvost-m@mail.ru>
2023-01-25 22:46:45 +0300

    lesson_2

--- app/controller/pages.go
+++ app/controller/pages.go
@@ -1,14 +1,26 @@
 package controller
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"path/filepath"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func StartPage(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	text := "Приветствую тебя на стартовой странице этого сайта!"
-	fmt.Fprint(rw, text)
+	path := filepath.Join("app", "public", "html", "startStaticPage.html")
+
+	tmpl, err := template.ParseFiles(path)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+
+	err = tmpl.Execute(rw, nil)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
 
 }
--- app/controller/users.go
+++ app/controller/users.go
@@ -1,8 +1,9 @@
 package controller
 
 import (
-	"encoding/json"
+	"html/template"
 	"net/http"
+	"path/filepath"
 
 	"example.com/siteGolang/app/model"
 	"github.com/julienschmidt/httprouter"
@@ -10,16 +11,23 @@ import (
 
 func GetUsers(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	user, err := model.GetAllUsers()
-
 	if err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
 
-	err = json.NewEncoder(rw).Encode(user)
+	main := filepath.Join("app", "public", "html", "usersDynamicPage.html")
+	common := filepath.Join("app", "public", "html", "common.html")
+
+	tmpl, err := template.ParseFiles(main, common)
 	if err != nil {
 		http.Error(rw, err.Error(), 400)
 		return
 	}
 
+	err = tmpl.ExecuteTemplate(rw, "users", user)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
 }
